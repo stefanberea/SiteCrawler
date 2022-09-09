7ba30219a71e974f57f0900d32a6765dabade83d
stefan.berea <stberea@outlook.com>
2022-09-09 17:40:25 +0200

    Added Cass for handling html documents and requests.

---
+++ HttpHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace SiteCrawler
+{
+    public class HttpHelper
+    {
+        public static string GetSource(string baseUrl)
+        {
+            string result = string.Empty;
+            if (!IsUrlValid(baseUrl))
+                return result;
+
+            using (HttpClient client = new())
+            {
+                using HttpResponseMessage response = client.GetAsync(baseUrl).Result;
+                using HttpContent content = response.Content;
+                result = content.ReadAsStringAsync().Result;
+            }
+
+            return result;
+        }
+
+        private static bool IsUrlValid(string baseUrl)
+        {
+            return Uri.IsWellFormedUriString(baseUrl, UriKind.Absolute);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using HtmlAgilityPack;
+using SiteCrawler;
+
 Console.WriteLine("Hello, World!");
+
+var baseUrl = "http://google.com/";
+var htmlSource = HttpHelper.GetSource(baseUrl);
+
+var document = new HtmlDocument();
+document.LoadHtml(htmlSource);
+var urls = document.DocumentNode.SelectNodes("//a");
+foreach (var url in urls)
+{
+    //var parsedUrl = HttpHelper.ParseUrl();
+    Console.WriteLine(url.Attributes["href"].Value);
+}
