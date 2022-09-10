1158719613119bd997f95b8d302624f78d8395e5
stefan.berea <stberea@outlook.com>
2022-09-10 10:15:43 +0200

    Fixed renaming issues.

---
+++ SiteCrawler/SiteCrawler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using SiteCrawler;
+
+namespace SiteCrawler
+{
+    public class SiteCrawler
+    {
+        public void f()
+        {
+        }
+    }
+}
